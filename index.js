cd3235fa35c70dbad010eb88a0af9605af5ae6f0
Cayleb West <cwest@anamosa.k12.ia.us>
2026-04-20 11:31:48 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Define the string with required characters
+let introduction = "Hello there!\n\tThis is a string with a single quote (') and a double quote (\") and a backslash (\\).";
+
+// Assign it to the HTML element with id "result"
+document.getElementById("result").innerHTML = introduction;
